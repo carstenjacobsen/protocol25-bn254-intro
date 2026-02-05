ec7791c3349436b56be8e8450546866b42f7513b
Carsten Jacobsen <carsten.net@gmail.com>
2026-02-05 11:54:32 -0800

    Update test.rs

--- contracts/multiply/src/test.rs
+++ contracts/multiply/src/test.rs
@@ -8,20 +8,28 @@ fn test_multiply_point() {
   let env = Env::default();
   let contract_id = env.register(Contract, ());
   let client = ContractClient::new(&env, &contract_id);
-  
+
+  // Create a byte array for a point (1,2)
   let point_bytes: [u8; 64] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,  // X
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,  // Y
   ];
-  
+
+  // Call the multiply_point() contract function with the 
+  // (1,2) point and the value `1` as the multiplier. By
+  // Using `1` as the multiplier mean we can expect the 
+  // result to be the same as the point provided
   let result = client.multiply_point(
     &Bn254G1Affine::from_array(&env, &point_bytes),
     &U256::from_u32(&env, 1)
   );
 
+  // Convert the byte array to a Bn254G1Affine point
   let point = Bn254G1Affine::from_array(&env, &point_bytes);
-  
+
+  // Check if the multiply_point() function returns the (1,2) 
+  // point after the multiplication
   assert_eq!(result, point);
 }
