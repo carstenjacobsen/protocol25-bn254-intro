04d471780bbba83140c41451a93ca8720ee60bc7
Carsten Jacobsen <carsten.net@gmail.com>
2026-02-06 23:28:32 -0800

    Update lib.rs

--- contracts/add/src/lib.rs
+++ contracts/add/src/lib.rs
@@ -7,23 +7,7 @@ pub struct Contract;
 
 [contractimpl]
 impl Contract {
-  pub fn add_points(env: Env) -> Bn254G1Affine {
-    // Create a byte array for a point (1,2)
-    let point_bytes: [u8; 64] = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,  // X
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,  // Y
-    ];
-
-    // Create a point G on the curve from the byte array
-    let point = Bn254G1Affine::from_array(&env, &point_bytes);
-
-    // Negate the point: -G has same x but negated y
-    let neg_point = -point.clone();
-
-    // Add the two points (G+(-G)) and the expected returned  
-    // result is a (0,0) point
-    env.crypto().bn254().g1_add(&point, &neg_point)
+  pub fn add_points(env: Env, point_1: Bn254G1Affine, point_2: Bn254G1Affine) -> Bn254G1Affine {
+      env.crypto().bn254().g1_add(&point_1, &point_2)
   }
 }
