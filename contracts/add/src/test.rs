ef6f9dc8dbe40ae6b9fc1ae4d797f8c3029b6605
Carsten Jacobsen <carsten.net@gmail.com>
2026-02-06 16:30:34 -0800

    Update test.rs

--- contracts/add/src/test.rs
+++ contracts/add/src/test.rs
@@ -9,11 +9,29 @@ fn test_add_points() {
   let contract_id = env.register(Contract, ());
   let client = ContractClient::new(&env, &contract_id);
 
-  // Call the contract add_points()
-  let result = client.add_points();
+  // Create a byte array for a point (1,2)
+  let point_bytes: [u8; 64] = [
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+  ];
+
+  // Create a point on the curve from the byte array
+  let point = Bn254G1Affine::from_array(&env, &point_bytes);
+  std::println!("Point B: {:?}", &point);
+
+  // Negate the point: -G has same x but negated y
+  let neg_point = -point.clone();
+  std::println!("Point -B: {:?}", &neg_point);
+
+  // Call the function and get the result of the addition
+  let result = client.add_points(&point, &neg_point);
+
   // Create a (0,0) point on the curve
   let zero_point = Bn254G1Affine::from_array(&env, &[0u8; 64]);
-  // Check if the add_points() function returns the (0,0) point
+  
+  // Check if the add_points() function returns a (0,0) point
   assert_eq!(result, zero_point);
 }
   
