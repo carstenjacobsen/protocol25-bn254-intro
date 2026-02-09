5edf159a5694ae41442a655e60420ff26a976352
Carsten Jacobsen <carsten.net@gmail.com>
2026-02-09 07:15:28 -0800

    Update lib.rs

--- contracts/add/src/lib.rs
+++ contracts/add/src/lib.rs
@@ -5,7 +5,7 @@ use soroban_sdk::crypto::bn254::{Bn254G1Affine};
 #[contract]
 pub struct Contract;
 
-[contractimpl]
+#[contractimpl]
 impl Contract {
   pub fn add_points(env: Env, point_1: Bn254G1Affine, point_2: Bn254G1Affine) -> Bn254G1Affine {
       env.crypto().bn254().g1_add(&point_1, &point_2)
